4341da52cf1b7abe4eadc7b229b5060fbb6897b1
siddiquimaha <siddiqui.mahajabeen@gmail.com>
2025-04-29 19:35:42 -0700

    add redirects into public folder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,15 +3,23 @@ import './App.css';
 
 function App() {
   return (
-    <>
-
-    <div className="App justify-center text-center font-bold py-10">
-      <h1 className='text-red-400'>WELCOME TO SAYLANI MASS IT TRAINNING SYSTEM</h1>
-      <p>
-          <Link to={'login'}>Already have an account? </Link><Link to={'register'}>Sign up</Link>
-        </p>
+    <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100 px-4">
+      <div className="bg-white shadow-md rounded-lg p-8 max-w-md w-full text-center">
+        <h2 className="text-3xl sm:text-3xl font-bold text-red-500 mb-4">
+          Welcome to Saylani Employess Dashboard System
+        </h2>
+        <div className="space-x-4">
+          <Link to="/login" className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded transition">
+            Login
+          </Link>
+          <Link
+            to="/register"
+            className="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded transition">
+            Sign Up
+          </Link>
+        </div>
+      </div>
     </div>
-    </>
   );
 }
 
